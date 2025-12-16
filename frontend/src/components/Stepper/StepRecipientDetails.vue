136c69e4543c5c42204dc4e637ead3fc12614112
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-12-16 16:47:04 -0300

    fe add: StepRecipientDetails to Stepper

---
+++ frontend/src/components/Stepper/StepRecipientDetails.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+  import type { GiftRequest } from '@/types/GiftRequests';
+
+  const props = defineProps<{ modelValue: GiftRequest }>();
+</script>
+
+<template>
+  <v-container>
+    <h3 class="text-h6 mb-4">The Recipient</h3>
+    <v-row>
+      <v-col cols="12" md="4">
+        <v-text-field
+          v-model.number="props.modelValue.recipientAge"
+          label="Age"
+          type="number"
+          variant="outlined"
+        />
+      </v-col>
+
+      <v-col cols="12" md="8">
+        <v-select
+          v-model="props.modelValue.relationship"
+          label="Relationship"
+          :items="['Friend', 'Spouse', 'Parent', 'Sibling', 'Colleague', 'Child']"
+          variant="outlined"
+        />
+      </v-col>
+
+      <v-col cols="12">
+        <v-text-field
+          v-model="props.modelValue.location"
+          label="Location"
+          placeholder="City, State or Country"
+          variant="outlined"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
