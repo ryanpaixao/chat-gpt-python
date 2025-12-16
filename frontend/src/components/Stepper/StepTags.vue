917f63f7c8b2b8748e971453d9e138aa189ae132
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-12-16 17:31:16 -0300

    fe add: StepTags to Stepper

---
+++ frontend/src/components/Stepper/StepTags.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: string[];
+  title: string;
+  label: string;
+  placeholder: string;
+  color?: string;
+}>();
+
+const emit = defineEmits(['update:modelValue']);
+</script>
+
+<template>
+  <v-container>
+    <h3 class="text-h6 mb-4">{{ title }}</h3>
+    <v-combobox
+      :model-value="modelValue"
+      @update:model-value="emit('update:modelValue', $event)"
+      :label="label"
+      :placeholder="placeholder"
+      chips
+      multiple
+      closable-chips
+      variant="outlined"
+      :color="color"
+      hint="Press enter to add multiple items"
+      persistent-hint
+    />
+  </v-container>
+</template>
